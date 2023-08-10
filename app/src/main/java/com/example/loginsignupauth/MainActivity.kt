0b5c0bc94e5781b1e8a37baa69024b32c8f61c68
jatinkumar987 <mishu09871@gmail.com>
2023-08-10 18:59:21 +0530

    splash screen,chat activity, image activity

--- app/src/main/java/com/example/loginsignupauth/MainActivity.kt
+++ app/src/main/java/com/example/loginsignupauth/MainActivity.kt
@@ -1,11 +1,22 @@
 package com.example.loginsignupauth
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.loginsignupauth.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.generateImage.setOnClickListener{
+            startActivity(Intent(this,ImageGenerateActivity::class.java))
+        }
+        binding.generateImage.setOnClickListener{
+            startActivity(Intent(this,ChatActivity::class.java))
+        }
     }
 }
